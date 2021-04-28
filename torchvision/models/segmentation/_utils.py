928ac861130ff53137e02eb4953acd0e8033b585
edxsx <65843134+edxsx@users.noreply.github.com>
2021-04-28 00:18:44 -0400

    Update _utils.py

--- torchvision/models/segmentation/_utils.py
+++ torchvision/models/segmentation/_utils.py
@@ -7,11 +7,11 @@ from torch.nn import functional as F
 class _SimpleSegmentationModel(nn.Module):
     __constants__ = ['aux_classifier']
 
-    def __init__(self, backbone, classifier, aux_classifier=None):
+    def __init__(self, backbone, classifier, devices, aux_classifier=None):
         super(_SimpleSegmentationModel, self).__init__()
-        self.backbone = backbone.to(device[0])
-        self.classifier = classifier.to(device[1])
-        self.aux_classifier = aux_classifier.to(device[1])
+        self.backbone = backbone.to(devices[0])
+        self.classifier = classifier.to(devices[1])
+        self.aux_classifier = aux_classifier.to(devices[1])
 
     def forward(self, x):
         input_shape = x.shape[-2:]
@@ -20,13 +20,13 @@ class _SimpleSegmentationModel(nn.Module):
 
         result = OrderedDict()
         x = features["out"]
-        x = self.classifier(x.to(device[1]))
+        x = self.classifier(x.to(devices[1]))
         x = F.interpolate(x, size=input_shape, mode='bilinear', align_corners=False)
         result["out"] = x
 
         if self.aux_classifier is not None:
             x = features["aux"]
-            x = self.aux_classifier(x.to(device[1]))
+            x = self.aux_classifier(x.to(devices[1]))
             x = F.interpolate(x, size=input_shape, mode='bilinear', align_corners=False)
             result["aux"] = x
 
